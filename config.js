1598a1a771a9e18d83204f89dcbc7e297df27ba0
ramloul24 <43078223+ramloul24@users.noreply.github.com>
2020-05-03 18:13:33 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    botToken: 'xoxb-4218468268-1095956338624-tcahUr1aXcnMXpdNPd0prRli',
-    userIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ', 'UM9R9R4Q6', 'UGXR887F1', 'U8NA38VE3', 'U841MF87L',
-              'UP5V43E02', 'US9SS9QGH', 'UDHA302PJ', 'UA92UUKFC', 'U046EDS7Y', 'UM9R9RA7Q', 'US79V3WUS'],
-    testIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ'],
-    webhookURL: 'https://hooks.slack.com/services/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',
-    test: 'abcd'
-}

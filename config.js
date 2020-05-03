f2b656d54eb55c721398509c871706b61c5b8ce0
Girish <ramloul24@gmail.com>
2020-05-03 18:12:10 -0400

    test gitignore

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     userIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ', 'UM9R9R4Q6', 'UGXR887F1', 'U8NA38VE3', 'U841MF87L',
               'UP5V43E02', 'US9SS9QGH', 'UDHA302PJ', 'UA92UUKFC', 'U046EDS7Y', 'UM9R9RA7Q', 'US79V3WUS'],
     testIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ'],
-    webhookURL: 'https://hooks.slack.com/services/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'
+    webhookURL: 'https://hooks.slack.com/services/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',
+    test: 'abcd'
 }

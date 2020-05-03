a4b85140f3ba0850b772f3b8f3b5e1f5db5ce6b0
Girish <ramloul24@gmail.com>
2020-05-03 18:09:32 -0400

    Remove config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    botToken: 'xoxb-4218468268-1095956338624-DbxTIiNVeGFlY4zM5GVTxgRf',
+    botToken: 'xoxb-4218468268-1095956338624-tcahUr1aXcnMXpdNPd0prRli',
     userIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ', 'UM9R9R4Q6', 'UGXR887F1', 'U8NA38VE3', 'U841MF87L',
               'UP5V43E02', 'US9SS9QGH', 'UDHA302PJ', 'UA92UUKFC', 'U046EDS7Y', 'UM9R9RA7Q', 'US79V3WUS'],
     testIds: ['UPJNXUALV', 'U4LK993NZ', 'UMSH9PLRJ'],
-    webhookURL: 'https://hooks.slack.com/services/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'
+    webhookURL: 'https://hooks.slack.com/services/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'
 }
